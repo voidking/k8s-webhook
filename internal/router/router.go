23a2792beb0c9596a285b3b19eea34ca4d608a98
voidking <voidking@qq.com>
2024-10-14 16:58:32 +0800

    feature: 添加探活接口

--- internal/router/router.go
+++ internal/router/router.go
@@ -11,9 +11,14 @@ func SetRouter() (*gin.Engine, error) {
 	nsac := api.NamespaceAdmission{}
 	podac := api.PodAdmission{}
 
+	// 根路由
 	router.Any("/", nsac.HandleAdmission)
+	// 探活路由
+	router.GET("/healthz", api.Healthz)
+	// pod相关路由
 	podRouter := router.Group("/pod")
 	podRouter.POST("/mutating", podac.HandleMutatingAdmission)
 	podRouter.POST("/validating", podac.HandleValidatingAdmission)
+
 	return router, nil
 }
